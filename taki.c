317effc11e3fc157908ec5f83f95fb0da2777334
takahikohasegawa <takahikohasegawa@10.64.21.125>
2017-02-21 11:36:26 +0000

    updated

--- taki.c
+++ taki.c
@@ -15,11 +15,36 @@ int check_button(int button){
     int b0=0, b1=0, b2=0, b3=0, b4=0, b5=0, b6=0, b7=0; //8 buttons
     
     
+            
     
+    return 0;  
 }
 
 int check_switch(int switchn){
-    int s0=0, s1=0, s2=0;       // 3 switches excluding the top one 
+    int Read, Mask, Store;       // 3 switches excluding the top one 
+    
+    Read = PORTG;                //Reading PortG
+    Mask = 00000001;            //Mask off with the value
+    Store = Mask & Read;        //And it to isolate 
+    
+    if (Store != 1)
+    {
+        Mask = 00000010;
+        Store = Mask & Read; 
+        if (Store != 1)
+        {
+           Mask = 00000100;
+           Store = Mask & Read; 
+           if (Store != 1)
+           {
+               Store = 0; 
+           }
+        }
+        
+    }
+    return Store;
+   
+    
     
     
     
