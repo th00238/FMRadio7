8f00d26f5c8d174fec50e0b7dabd8869574875e6
takahikohasegawa <takahikohasegawa@10.64.21.125>
2017-02-21 11:41:44 +0000

    updated

--- taki.h
+++ taki.h
@@ -12,6 +12,14 @@
 
 // Switches 0 1 2 are RG0 RG1 RG2
 // Buttons 0 1 2 3 4 5 6 7 are RA0 RA1 RE4 RE5 RE6 RE7 RG3 RG4
+// 4 Switches including power switch
+// Switch 1 (0) = stereo/ mono
+// Switch 2 (1) = Mode 
+// Switch 3 (2) = Mute 
+// Switch 4 (3) = Power, no to be programmed 
+// Button 0&1 = Volume 
+// Button 2&3 = Channel 
+// Button 4&5, 6, 7 = Storage button 
 
 void set_lcd(int khz); //in 100 khz
 int check_button(int button);
