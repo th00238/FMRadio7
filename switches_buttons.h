2662f04b8e77effa79f4be15de94c88fd2e9a3fb
matthew <matthew@matt-switch11>
2017-02-21 12:04:10 +0000

    Switches

---
+++ switches_buttons.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   switches_buttons.h
+ * Author: Matthew Mould
+ *
+ * Created on 21 February 2017, 12:00
+ */
+
+#ifndef SWITCHES_BUTTONS_H
+#define	SWITCHES_BUTTONS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define MUTE_SWITCH 0
+#define STEREO_SWITCH 1
+#define TUNE_SEEK_SWITCH 2
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SWITCHES_BUTTONS_H */
+
