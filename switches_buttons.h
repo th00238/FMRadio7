2ea2a3c32c1a6cc197599c91890702c1d3e57f6a
matthew <matthew@matt-switch11>
2017-03-06 22:33:53 +0000

    Complete buttons

--- switches_buttons.h
+++ switches_buttons.h
@@ -12,9 +12,21 @@
 extern "C" {
 #endif
 
-#define MUTE_SWITCH 0
-#define STEREO_SWITCH 1
-#define TUNE_SEEK_SWITCH 2
+// Values set to 9 until correct values are known
+// Switches
+#define MUTE_SWITCH 9
+#define STEREO_SWITCH 9
+#define TUNE_SEEK_SWITCH 9
+
+// Buttons
+#define VOLUP_BUTTON 9
+#define VOLDOWN_BUTTON 9
+#define TUNEUP_BUTTON 9
+#define TUNEDOWN_BUTTON 9
+#define PRES1_BUTTON 9
+#define PRES2_BUTTON 9
+#define PRES3_BUTTON 9
+#define PRES4_BUTTON 9
 
 
 #ifdef	__cplusplus
