a4b8cce6d087aa1128969f0f969c4134d909a6e4
takahikohasegawa <takahikohasegawa@10.228.2.96>
2017-03-07 15:14:34 +0000

    updated

--- taki.c
+++ taki.c
@@ -9,16 +9,14 @@
 void Segment0(char segNumber);
 void Segment1(char segNumber);
 void Segment2(char segNumber);
+void Segment3(char segNumber);
 
-
-void set_lcd(int khz, char segNumber, int LCDSegment){
-    
-    //if khz is zero, clear screen 
-    
-    
-    int LCDDATA0 = 0b11111111; // turn on LCD Screen
+void ChangeSegment(char segNumber, int LCDSegment)
+{
+   /* int LCDDATA0 = 0b11111111; // turn on LCD Screen
     int LCDDATA1 = 0b11111111;
     int LCDDATA2 = 0b11111111;
+    int LCDDATA3 = 0b11111111;*/
 
             //This function called to switch between sub-functions
         switch (LCDSegment)
@@ -32,64 +30,65 @@ void set_lcd(int khz, char segNumber, int LCDSegment){
             case 2:
                 Segment2(segNumber);
                 break;
-        
+            case 3: 
+                Segment3(segNumber);
+                break;
+                
         }
-            
-
+         
+}
+void set_lcd(int khz){
     
-    /*int NumberIntoMhz, WriteLCD;
-    int one, ten, thou, tenth; // gets 1000 then outputs 100,0 
-    
-    
-    
-    NumberIntoMhz = khz/100; 
-    if (NumberIntoMhz > 10 && NumberIntoMhz < 100)// channel = 1000
+    int i;
+    for (i=0; i=<3; i++)
     {
-        WriteLCD = ',0';
+        if (khz != 0 )
+        {
+            ChangeSegment((khz%10), i);
+             // divide by 10 and get the remainder 
+            khz /= 10;
+        }
+        else
+            Segment0(0, i);
+        
     }
-      
-    one = 001;
-    ten = 010;
-    thou = 100,0;
-    tenthou = 100,00;*/
-   
+
 }
 void Segment0(char segNumber)
 {
-    int LCDDATA0;
     //switch between 9 numbers 
     switch (segNumber)
     {
         case 0:
-            LCDDATA0 = 10111111;
+            LCDDATA0 = 0b10111111;
             break;
             
         case 1:
-            LCDDATA0 = 10001100;
+            LCDDATA0 = 0b10001100;
             break;
         case 2:
-            LCDDATA0 = 11011011;
+            LCDDATA0 = 0b11011011;
             break;
         case 3:
-            LCDDATA0 = 11011110;
+            LCDDATA0 = 0b11011110;
             break;
         case 4:
-            LCDDATA0 = 11011110;
+            LCDDATA0 = 0b11011110;
             break; 
         case 5:
-            LCDDATA0 = 11110110;
+            LCDDATA0 = 0b11110110;
             break; 
         case 6:
-            LCDDATA0 = 11110111;
+            LCDDATA0 = 0b11110111;
             break;
         case 7:
-            LCDDATA0 = 10111100;
+            LCDDATA0 = 0b10011100;
             break;
         case 8:
-            LCDDATA0 = 11111111;
+            LCDDATA0 = 0b11111111;
             break;
         case 9:
-            LCDDATA0 = 11111100;
+            LCDDATA0 = 0b11111100;
             break;
 
     }
@@ -97,82 +96,118 @@ void Segment0(char segNumber)
 }
 void Segment1(char segNumber)
 {
-    int LCDDATA1;
     switch (segNumber){
         case 0:
-            LCDDATA1 = 10111111;
+            LCDDATA1 = 0b10111111;
             break;
             
         case 1:
-            LCDDATA1 = 10001100;
+            LCDDATA1 = 0b10001100;
             break;
         case 2:
-            LCDDATA1 = 11011011;
+            LCDDATA1 = 0b11011011;
             break;
         case 3:
-            LCDDATA1 = 11011110;
+            LCDDATA1 = 0b11011110;
             break;
         case 4:
-            LCDDATA1 = 11011110;
+            LCDDATA1 = 0b11011110;
             break; 
         case 5:
-            LCDDATA1 = 11110110;
+            LCDDATA1 = 0b11110110;
             break; 
         case 6:
-            LCDDATA1 = 11110111;
+            LCDDATA1 = 0b11110111;
             break;
         case 7:
-            LCDDATA1 = 10111100;
+            LCDDATA1 = 0b10011100;
             break;
         case 8:
-            LCDDATA1 = 11111111;
+            LCDDATA1 = 0b11111111;
             break;
         case 9:
-            LCDDATA1 = 11111100;
+            LCDDATA1 = 0b11111100;
             break;
-
     }
 }
 
 void Segment2(char segNumber)
 {
-    int LCDDATA2;
     switch (segNumber){
         case 0:
-            LCDDATA2 = 10111111;
+            LCDDATA2 = 0b10111111;
             break;
             
         case 1:
-            LCDDATA2 = 10001100;
+            LCDDATA2 = 0b10001100;
             break;
         case 2:
-            LCDDATA2 = 11011011;
+            LCDDATA2 = 0b11011011;
             break;
         case 3:
-            LCDDATA2 = 11011110;
+            LCDDATA2 = 0b11011110;
             break;
         case 4:
-            LCDDATA2 = 11011110;
+            LCDDATA2 = 0b11011110;
             break; 
         case 5:
-            LCDDATA2 = 11110110;
+            LCDDATA2 = 0b11110110;
             break; 
         case 6:
-            LCDDATA2 = 11110111;
+            LCDDATA2 = 0b11110111;
             break;
         case 7:
-            LCDDATA2 = 10111100;
+            LCDDATA2 = 0b10011100;
             break;
         case 8:
-            LCDDATA2 = 11111111;
+            LCDDATA2 = 0b11111111;
             break;
         case 9:
-            LCDDATA2 = 11111100;
+            LCDDATA2 = 0b11111100;
             break;
-
     }
 }
 
+void Segment3(char segNumber)
+{
+    switch (segNumber){
+        case 0:
+            LCDDATA3 = 0b10111111;
+            break;
+            
+        case 1:
+            LCDDATA3 = 0b10001100;
+            break;
+        case 2:
+            LCDDATA3 = 0b11011011;
+            break;
+        case 3:
+            LCDDATA3 = 0b11011110;
+            break;
+        case 4:
+            LCDDATA3 = 0b11011110;
+            break; 
+        case 5:
+            LCDDATA3 = 0b11110110;
+            break; 
+        case 6:
+            LCDDATA3 = 0b11110111;
+            break;
+        case 7:
+            LCDDATA3 = 0b10011100;
+            break;
+        case 8:
+            LCDDATA3 = 0b11111111;
+            break;
+        case 9:
+            LCDDATA3 = 0b11111100;
+            break;
+        /*case 10:
+            LCDDATA3 = 0b00000001;
+            break;*/
+            
+    }
+}
 int check_button(int button){
     int ReadPortA,ReadPortE,ReadPortG, Mask, ButtonNumber;      //8 buttons RA0 RA1 RE4 RE5 RE6 RE7 RG3 RG4
     
